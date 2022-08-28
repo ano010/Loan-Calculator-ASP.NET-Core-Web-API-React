69e691e7d106d51b6fa40971074572a28deba26b
Anojan Satheesnathan <anojans@infyinnos.com>
2022-08-28 10:58:17 +0530

    Handle UI unexpected error

--- loan-calculator-ui/src/App.js
+++ loan-calculator-ui/src/App.js
@@ -1,11 +1,17 @@
-import "./App.css";
+import React from "react";
+import { ToastContainer } from "react-toastify";
 import LoanCalculator from "./components/loanCalculator";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <main className="loan-details-form">
-      <LoanCalculator />
-    </main>
+    <React.Fragment>
+      <ToastContainer />
+      <main className="loan-details-form">
+        <LoanCalculator />
+      </main>
+    </React.Fragment>
   );
 }
 
